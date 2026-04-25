4b586c22a167814320fe8e4eca236558538a391f
brian-the-hacker <brianshiru563@gmail.com>
2026-04-25 23:03:30 +0300

    deploy

--- app.py
+++ app.py
@@ -65,5 +65,6 @@ def create_app():
 app = create_app()
 
 if __name__ == '__main__':
+    port = int(os.environ.get('PORT', 5000))
     debug = os.environ.get('FLASK_ENV') == 'development'
-    app.run(debug=debug, host='0.0.0.0', port=5000)
+    app.run(debug=debug, host='0.0.0.0', port=port)
